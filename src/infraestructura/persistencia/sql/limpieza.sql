fbddb007ccec73fdd80f82314678bf18075bc10a
Nicolas Piñera <nicolaspinera@gmail.com>
2026-05-24 22:41:47 -0300

    Correccion: Limpieza de tablas

--- src/infraestructura/persistencia/sql/limpieza.sql
+++ src/infraestructura/persistencia/sql/limpieza.sql
@@ -15,7 +15,46 @@ DELETE FROM club
 WHERE nombre = 'Universitario';
 -- Limpieza tabla Jugadores
 DELETE FROM jugador
-WHERE idJugador IN (0, 11);
+WHERE (
+        nombre = 'pepe'
+        and apellido = 'argento'
+    )
+    OR (
+        nombre = 'antonio'
+        and apellido = 'argentinito'
+    )
+    OR (
+        nombre = 'Gabriel'
+        and apellido = 'batistuta'
+    )
+    OR (
+        nombre = 'LEONEL ANDRES'
+        and apellido = 'messi'
+    )
+    OR (
+        nombre = 'Pepa'
+        and apellido = 'arrigoni'
+    )
+    OR (
+        nombre = 'Sergio'
+        and apellido = 'aguero'
+    )
+    OR (
+        nombre = 'Julian'
+        and apellido = 'alvarez'
+    )
+    OR (
+        nombre = 'cristian'
+        and apellido = 'romero'
+    )
+    OR (
+        nombre = 'Nico'
+        and apellido = 'otamendi'
+    )
+    OR (
+        nombre = 'nahuel'
+        and apellido = 'molina'
+    );
 -- Limpieza tabla competencia
 DELETE FROM competencia
 WHERE nombre = 'PROVINCIAL U21';
