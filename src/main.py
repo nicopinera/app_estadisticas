283eebff0efbb4941b8fabcce8f232d62be8f6d1
Nicolas Piñera <nicolaspinera@gmail.com>
2026-05-24 23:46:49 -0300

    Agregado: Implementacion de logger

--- src/main.py
+++ src/main.py
@@ -1,17 +1,16 @@
 from infraestructura.persistencia.database_manager import SQLiteManager
+from infraestructura.logger import get_logger
 import config.rutas as r
 
+logger = get_logger(__name__)
+
 def main():
+    logger.info('Ejecutando orquestador principal')
     pruebaSQL = SQLiteManager(r.DB_FILE,r.SCHEMA_SQL,r.VISTA_SQL,r.SEED_SQL,r.CLEAR_SQL)
-    conexion_test = None
-    try:
-        conexion_test = pruebaSQL.connect()
-        pruebaSQL.inicializar_schema()
-        # pruebaSQL.cargar_seed()
-        # pruebaSQL.limpieza()
-    finally:
-        if conexion_test is not None:
-            conexion_test.close()
+    conexion_test = pruebaSQL.connect() # Genero conexion a DB
+    pruebaSQL.inicializar_schema() # Inicio db
+    pruebaSQL.close_connection() # Cierro conexion con DB
+    logger.info('Fin de programa')
 
 if __name__=="__main__":
     main()
