54a4a2b0cc925ae7f5751bb132a258733655ea68
Nicolas Piñera <nicolaspinera@gmail.com>
2026-06-16 20:27:16 -0300

    Agregado: Interfaz jugador

---
+++ src/dominio/repositorios/jugador_repositorio.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+
+class JugadorRepositorio(ABC):
+    "Maneja Jugador y JugadorClub"
+
+    @abstractmethod
+    def buscar_por_id(self, id_jugador: int) -> set | None:
+        "Busca un jugador por id"
+        pass
+
+    @abstractmethod
+    def buscar_por_dni(self, dni_jugador: int) -> set | None:
+        "Busca un jugador por DNI"
+        pass
+
+    @abstractmethod
+    def buscar_por_club(self, idClub: int) -> list:
+        "Busca todos los jugadores de un club"
+        pass
+
+    @abstractmethod
+    def guardar(
+        self, nombre: str, apellido: str, dni: int, anioNacimiento: int
+    ) -> None:
+        pass
+
+    @abstractmethod
+    def link_to_club(self, id_jugador: int, id_club: int) -> None:
+        "Vincula un jugador con un club, verificando link con otro club"
+        pass
+
+    @abstractmethod
+    def club_activo(self, id_jugador: int, id_club: int) -> str | None:
+        "Devuelve el club activo de un jugador"
+        pass
