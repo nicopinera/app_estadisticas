3f12aa31ca738d748831873baaf34d99e54e3424
Nicolas Piñera <nicolaspinera@gmail.com>
2026-07-12 23:08:50 -0300

    REFACTOR: Corrección de tipos de retorno en métodos del Repositorio Usuario

--- src/dominio/repositorios/usuario_repositorio.py
+++ src/dominio/repositorios/usuario_repositorio.py
@@ -1,14 +1,15 @@
 from abc import ABC, abstractmethod
+from entidades.usuario import Usuario
 
 
 class UsuarioRepositorio(ABC):
     @abstractmethod
-    def encontrar_por_mail(self, email: str) -> set:
+    def encontrar_por_mail(self, email: str) -> Usuario:
         "Busca un usuario por el mail"
         pass
 
     @abstractmethod
-    def encontrar_por_id(self, id: int) -> set:
+    def encontrar_por_id(self, id: int) -> Usuario:
         "Busca usuario por id"
         pass
 
