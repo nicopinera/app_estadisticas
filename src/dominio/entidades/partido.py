c9d2b00754e10117861a0470440d0c3885114cc9
Nicolas Piñera <nicolaspinera@gmail.com>
2026-07-13 18:48:06 -0300

    REFACTOR: Creacion de dataclasses para repositorio de Partidos

--- src/dominio/entidades/partido.py
+++ src/dominio/entidades/partido.py
@@ -3,9 +3,33 @@ from dataclasses import dataclass
 
 @dataclass
 class Partido:
-    pass
+    fecha: str
+    estadio: str | None
+    idCompetencia: int
+    idClubLocal: int
+    idClubVisitante: int
+    idPartido: int | None = None
 
 
 @dataclass
 class JugadorPartido:
-    pass
+    idJugador: int
+    idPartido: int
+    idClub: int
+    minutosJugados: int = 0
+    puntos: int = 0
+    t2c: int = 0
+    t2l: int = 0
+    t3c: int = 0
+    t3l: int = 0
+    t1c: int = 0
+    t1l: int = 0
+    rebotesDef: int = 0
+    rebotesOf: int = 0
+    asistencias: int = 0
+    recuperos: int = 0
+    perdidas: int = 0
+    taponesRecibidos: int = 0
+    taponesRealizados: int = 0
+    faltasRecibidas: int = 0
+    faltasCometidas: int = 0
