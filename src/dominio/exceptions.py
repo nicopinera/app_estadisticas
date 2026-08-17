7ad12ff85f95e56fbc0329339eaf7ae901819f23
Nicolas Piñera <nicolaspinera@gmail.com>
2026-08-16 23:05:55 -0300

    refactor: agregar espacios en blanco para mejorar la legibilidad del código en excepciones

--- src/dominio/exceptions.py
+++ src/dominio/exceptions.py
@@ -4,7 +4,10 @@
 class ErrorDeDominio(Exception):
     """Clase base para todas las excepciones de dominio."""
 
+    pass
+
 
 class DNIDuplicadoError(ErrorDeDominio):
     """Se lanza cuando se intenta guardar un jugador con un DNI ya registrado."""
 
+    pass
